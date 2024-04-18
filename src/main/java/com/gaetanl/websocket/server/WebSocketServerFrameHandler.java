1441cb23f9daa4ec9f41b5c08db8f19bf24d93b5
Gaetan LEU <gleu-presta@cora.fr>
2024-04-18 14:52:15 +0200

    Minor changes

--- src/main/java/com/gaetanl/websocket/server/WebSocketServerFrameHandler.java
+++ src/main/java/com/gaetanl/websocket/server/WebSocketServerFrameHandler.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012 The Netty Project
- *
- * The Netty Project licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
 package com.gaetanl.websocket.server;
 
 import static com.gaetanl.websocket.util.WebSocketUtil.Direction.INBOUND;
@@ -101,14 +86,4 @@ public class WebSocketServerFrameHandler extends SimpleChannelInboundHandler<Web
             ctx.channel().writeAndFlush(responseFrame);
         }
     }
-
-//    @Override
-//    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-//        if (evt instanceof WebSocketServerProtocolHandler.HandshakeComplete) {
-//            //Channel upgrade to websocket, remove WebSocketIndexPageHandler.
-//            ctx.pipeline().remove(WebSocketServerHttpHandler.class);
-//        } else {
-//            super.userEventTriggered(ctx, evt);
-//        }
-//    }
 }
